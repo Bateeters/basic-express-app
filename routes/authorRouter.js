33ccc259f3a8c39e71efe700078ab817276fe10f
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-12-18 16:24:48 -0500

    implemented the author controller in authorRouter.js

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 
+// importing controller for use
+const { getAuthorById } = require('../controller/authorController');
+
 const authorRouter = Router()
 
 authorRouter.get("/", (req, res) => res.send("All authors"));
-authorRouter.get("/:authorId", (req, res) => {
-    const { authorId } = req.params;
-    res.send(`Author ID: ${authorId}`);
-})
+
+// using the imported getAuthorById function to populate /authors/# route
+authorRouter.get("/:authorId", getAuthorById);
 
 module.exports = authorRouter;
