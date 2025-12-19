9314c94d8fde80d413d31dcb43c15f7734047041
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-12-19 13:13:34 -0500

    added try catch block for errors

--- controller/authorController.js
+++ controller/authorController.js
@@ -6,14 +6,24 @@ const db = require("../db");
 async function getAuthorById(req, res) {
     const { authorId } = req.params;
 
-    const author = await db.getAuthorById(Number(authorId));
+    // adding a try/catch block to handle errors
+    try {
+        const author = await db.getAuthorById(Number(authorId));
 
-    if (!author) {
-        res.status(404).send("Author not found");
-        return;
+        if (!author) {
+            res.status(404).send("Author not found");
+            return;
+        }
+
+        res.send(`Author Name: ${author.name}`);
+    } catch (error) {
+        console.lerror("Error retrieving author:", error);
+        res.status(500).send("internal Server Error");
+
+        // or we can call next(error) instead of sending a response here
+        // Using `next(error)` however will only render an error page in the express' default view and respond with the whole html to the client.
+        // So we will need to create a special type of middleware function if we want a different response and we will get to that... next.
     }
-
-    res.send(`Author Name: ${author.name}`);
 };
 
 module.exports = { getAuthorById };
